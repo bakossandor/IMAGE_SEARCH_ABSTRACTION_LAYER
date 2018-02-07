34cd0f3f7a79f7a2a32c473976f77e08335bb53e
sandor.bakos <bakos.webd@gmail.com>
2018-02-07 20:35:25 +0100

    all routes live, need to set up the rules

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 //npm modules
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const MongoClient = require("mongodb").MongoClient;
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const MongoClient = require("mongodb").MongoClient;
 const app = express();
-const routes = require('express').Router();
+// const routes = require('express').Router();
 
 // middlewares
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
 //testing the app
-app.get("/", (req, res) => {
-    res.send("working");
-});
+const apiBaseRoot = require("./api/routes/api_landing_page");
+const apiImageSearch = require("./api/routes/api_image_search");
+const apiHistory = require("./api/routes/api_history");
+
+app.use("/", apiBaseRoot);
+app.use("/api/imagesearch", apiImageSearch);
+app.use("/api/history", apiImageSearch);
 
 app.listen(3000, () => {
     console.log("server is listening");
