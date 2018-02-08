5dc633521408f82502e91425d9e63c57a93f7050
sandor.bakos <bakos.webd@gmail.com>
2018-02-08 23:05:20 +0100

    the image search api is done v 0.0.2

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # IMAGE_SEARCH_ABSTRACTION_LAYER
 freeCodeCamp Project - IMAGE_SEARCH_ABSTRACTION_LAYER
 
-##08FEB2017
+##08FEB2017 - v 0.0.2
 - error handling
 - found out how to get the query object
 - mlab connection with mongoose
+- image search api is done with the offset
+ - external api is used called PIXABAY
+ - lodash pick library is included - powerful
 
-##07FEB2017
+##07FEB2017 - v 0.0.1
 - project init
 - base routes set up
--- api/routes/api_image_search.js
+++ api/routes/api_image_search.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const bodyParser = require("body-parser");
+const pick = require('lodash.pick');
 
 router.get("/:search", (req, res, next) => {
-    res.status(200).json({
-        requestUrlParameter: req.params.search,
-        requestQuery: req.query
-    });
+    //get url request related with the image wish
+    var searchedImage = req.params.search;
+    var padding = req.query.offset;
+    //the pixabay api request url settings
+    var resultMax = 10;
+    if (padding && padding <= 100) {
+        resultMax = padding;
+    } else if (padding && padding > 100) {
+        resultMax = 100;
+    }
+    var url = `https://pixabay.com/api/?key=${process.env.PIXABAY_KEY}&q=dogs&per_page=10`
+    
+    request(url, (error, response, body) => {
+        var responseBody = JSON.parse(body);
+        var hits = responseBody.hits;
+        var theApiResult = hits.map((element) => {
+            return pick(element, "tags", "pageURL", "previewURL", "webformatURL");
+        });
+        res.json(theApiResult);
+    })
 });
 
 module.exports = router;
